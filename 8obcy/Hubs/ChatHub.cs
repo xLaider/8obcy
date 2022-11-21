3bed9ec851c08f93b49c88bf0d1b1cc1075a61e8
Mateusz Błażków <mateuszblazkow1@gmail.com>
2022-11-21 19:32:28 +0100

    Add project files.

---
+++ 8obcy/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace _8obcy.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",username, message);
+        }
+    }
+}
